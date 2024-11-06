bd77ecaf0dd55323a98b45971141f9db91473531
TLT92 <85993347+TLT92@users.noreply.github.com>
2024-11-06 09:04:39 +0200

    Added keyboard support, simple styling and small advanced functions, e.g. decimals.

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
-let firstNumber;
-let operator;
-let secondNumber;
+let firstNumber = "";
+let operator = "";
+let secondNumber = "";
 let result;
 
 function operate(operator, firstNumber, secondNumber) {
@@ -44,30 +44,94 @@ Create the functions that populate the display when you click the digit buttons.
 You should store the content of the display (the number) in a variable for use in the next step.
 */
 
+
+function extractNumbers() {
+  // Split string input to extract numbers & store them as numbers.
+  let display = document.querySelector("#display");
+  displayText = display.textContent
+  let splitString = displayText.split(/[-+*/]/);
+
+  splitString[0] = Number(splitString[0]);
+  splitString[1] = Number(splitString[1]);
+
+  return splitString
+}
+
 // Function to show user input on "display" of calculator.
 function populateDisplay(userInput) {
   let display = document.querySelector("#display");
+  if (userInput === "←" || userInput === "Backspace") {
+    display.textContent = display.textContent.slice(0, display.textContent.length - 1);
+    return;
+  }
+
+  if (userInput === ".") {
+    // Split string input to extract numbers & store them as numbers.
+    displayText = display.textContent
+
+    if (displayText.includes("+") || displayText.includes("-") || displayText.includes("*") || displayText.includes("/")) {
+      let numbers = extractNumbers();
+      let currentNumber = numbers[1];
+      if (currentNumber.includes(".")) {
+        return;
+      }
+    } else {
+      if (displayText.includes(".")) {
+        return
+      }
+    }
+  }
+
   display.textContent = display.textContent + userInput;
 
-  // If input is operator, store that operator in variable.
-  if(userInput === "+" || userInput === "-" || userInput === "*" || userInput === "/") {
-    operator = userInput;
+  if (operator === "") {
+    // If input is operator, store that operator in variable.
+    if (userInput === "+" || userInput === "-" || userInput === "*" || userInput === "/") {
+      operator = userInput;
+    }
+  } else if (userInput === "+" || userInput === "-" || userInput === "*" || userInput === "/") {
+    if (display.textContent[display.textContent.length - 2] === "+" || display.textContent[display.textContent.length - 2] === "-" || display.textContent[display.textContent.length - 2] === "*" || display.textContent[display.textContent.length - 2] === "/") {
+      display.textContent = display.textContent.slice(0, display.textContent.length - 2);
+      display.textContent = display.textContent + userInput;
+    } else {
+      display.textContent = display.textContent.slice(0, display.textContent.length - 1);
+    }
   }
 }
 
 function transformInput() {
+  debugger
   // Split string input to extract numbers & store them as numbers.
   let display = document.querySelector("#display");
   displayText = display.textContent
-  let splitString = displayText.split(/[-+*]/);
+  let splitString = displayText.split(/[-+*/]/);
   debugger
   firstNumber = Number(splitString[0]);
   secondNumber = Number(splitString[1]);
 
-  // Calculate result, display it & reset everything.
-  operate(operator, firstNumber, secondNumber);
-  clearDisplay();
-  populateDisplay(result);
+  debugger
+  if (operator === "/" && secondNumber === 0) {
+    clearDisplay();
+    display.textContent = "ERROR, DIVIDING BY 0 IS NOT ALLOWED!";
+  } else {
+    if (splitString[1] !== undefined) {
+      // Calculate result, display it & reset everything.
+      operate(operator, firstNumber, secondNumber);
+      clearDisplay();
+  
+      // If result has too many decimals for display, round down.
+      let stringResult = result.toString();
+      if (stringResult.includes(".")) {
+        let splitString = stringResult.split(".");
+        let decimals = splitString[1];
+        if (decimals.length > 6) {
+          result = Math.round((a + Number.EPSILON)*1000000)/1000000;
+          result = Number(result);
+        }
+      }
+    populateDisplay(result);
+    }
+  }
 }
 
 function clearDisplay() {
@@ -86,6 +150,33 @@ numberButtons.forEach(numberButton => {
   });
 });
 
+// Add key "support" to all buttons whose press are there to populate display.
+let displayButtons = document.querySelectorAll(".display-button");
+displayButtons.forEach(displayButton => {
+  displayButton.addEventListener("keydown", (event) => {
+    debugger
+    if (event.key === displayButton.textContent) {
+      populateDisplay(displayButton.textContent);
+    }
+  });
+});
+
+document.addEventListener("keydown", (event) => {
+  if (event.key === " ") {
+    return;
+  }
+  
+  let pressedKey = Number(event.key);
+  // Check if pressed key is not a number (a regular comparison like so "xyz !== NaN" does not work since any comparison with NaN returns false)
+  if (!isNaN(pressedKey) || event.key === "." || event.key === "+" || event.key === "-" || event.key === "*" || event.key === "/" || event.key === "Backspace") {
+    populateDisplay(event.key);
+  }
+
+  if (event.key === "Enter") {
+    transformInput();
+  }
+});
+
 // Add event listeners to all operation buttons.
 let operationButtons = document.querySelectorAll(".operation-button");
 operationButtons.forEach(operationButton => {
@@ -100,7 +191,7 @@ clearButton.addEventListener("click", clearDisplay);
 let calculateButton = document.querySelector("#calculate-button");
 calculateButton.addEventListener("click", transformInput);
 
-/*
+/* CLEARER? syntax of adding event listeners.
 // Iterate over each number button using forEach
 numberButtons.forEach(function(numberButton) { 
   // Add a click event listener to each number button
@@ -110,4 +201,8 @@ numberButtons.forEach(function(numberButton) {
     populateDisplay(buttonText); // Populate the display with the button text
   });
 });
+
+
+NEXT TO DOS:
+Add keyboard support!
 */
